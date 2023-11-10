b5d7e06293736694c1106dcf286ed5526042599e
Marco0201 <marcoperz0201@gmail.com>
2023-11-09 17:09:46 -0800

    changed filepath

--- networkhuntproject/networkhuntproject/settings.py
+++ networkhuntproject/networkhuntproject/settings.py
@@ -193,7 +193,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static/networkhuntproject'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
